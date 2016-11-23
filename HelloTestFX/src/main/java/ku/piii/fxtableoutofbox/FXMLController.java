0a3a7a79898013093c8f25276d8e0f353a22f080
darkquake93 <daniel.carnovale@gmail.com>
2016-11-23 09:57:59 +0000

    test commit

---
+++ HelloTestFX/src/main/java/ku/piii/fxtableoutofbox/FXMLController.java
@@ -0,0 +1,50 @@
+package ku.piii.fxtableoutofbox;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+@SuppressWarnings("restriction")
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private TextField selectedfiles;
+    
+    @FXML
+    private List<File> handleButtonAction(ActionEvent event) {
+Stage stage = new Stage(); 
+ FileChooser fileChooser = new FileChooser();
+ fileChooser.setTitle("Open Resource File");
+ fileChooser.getExtensionFilters().addAll(
+         new ExtensionFilter("Text Files", "*.txt"),
+         new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+         new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+         new ExtensionFilter("All Files", "*.*"));
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(stage);
+ if (selectedFiles != null) {
+    stage.centerOnScreen();
+ }
+selectedfiles.setText(selectedFiles.toString());
+ return selectedFiles;
+    }
+ 
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
